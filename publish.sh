e2349b4ab7dc694edebddbce2620f2c9a1a15bfb
awhui <dksp.ng.78@gmail.com>
2016-01-20 14:00:29 -0500

    blog update

--- publish.sh
+++ publish.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Have you undraft your post?"
+echo "######## Have you undraft your post?"
+echo
+echo "### push to blog repo now"
+echo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 git add -A
 git commit -m"blog update"
 git push origin master
+echo
+echo
+echo "### generate content to public folder and publish to github"
 hugo --theme=hyde
 cd public
 git add -A
